7d0d0e6558c52284a65b3ca32a45093e550d117c
jpushkar <66423213+jpushkar@users.noreply.github.com>
2020-07-28 21:40:35 +0530

    Create ec2-autoscaling-var.tf

---
+++ ec2-autoscaling-var.tf
@@ -0,0 +1,31 @@
+
+variable "region" {
+  default     = "ap-south-1"
+  type        = string
+  description = "Region of the VPC"
+}
+
+
+variable "cidr_block" {
+  default     = "10.0.0.0/16"
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnet_cidr_blocks" {
+  default     = ["10.0.0.0/24", "10.0.2.0/24"]
+  type        = list
+  description = "List of public subnet CIDR blocks"
+}
+
+variable "private_subnet_cidr_blocks" {
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
+  type        = list
+  description = "List of private subnet CIDR blocks"
+}
+
+variable "availability_zones" {
+  default     = ["ap-south-1a", "ap-south-1b"]
+  type        = list
+  description = "List of availability zones"
+}
